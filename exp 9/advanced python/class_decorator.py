550960e9415be623934f3db0f1c7bbb6c80377c2
chaitanyagulage <chaitanyagulage6@gmail.com>
2026-09-22 12:22:45 +0530

    Delete exp 9/advanced python directory

--- exp 9/advanced python/class_decorator.py
+++
@@ -1,9 +0,0 @@
-def fun(cls):
-    cls.class_name = cls.__name__
-    return cls
-
-@fun
-class Person:
-    pass
-
-print(Person.class_name)
--- exp 9/advanced python/closure.py
+++
@@ -1,8 +0,0 @@
-def outer_function(x):
-    def inner_function(y):
-        return x + y
-    return inner_function
-
-closure = outer_function(10)
-print(closure(5))
-print(closure(20))
